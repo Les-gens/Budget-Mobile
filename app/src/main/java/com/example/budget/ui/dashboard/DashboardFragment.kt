9c4e995cfdcf2dc2b80c9162c6ff53b1ba048439
Skiadram <nlcoffin@free.fr>
2021-05-23 18:23:53 +0200

    :sparkles: previous and next month

--- app/src/main/java/com/example/budget/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/budget/ui/dashboard/DashboardFragment.kt
@@ -1,19 +1,24 @@
 package com.example.budget.ui.dashboard
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.example.budget.R
+import androidx.navigation.fragment.findNavController
 
 class DashboardFragment : Fragment() {
 
     private lateinit var dashboardViewModel: DashboardViewModel
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
@@ -31,6 +36,20 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.currentMonth.observe(viewLifecycleOwner, Observer {
             textCurrentMonthView.text = it
         })
+
+        val previousMonthBtn = root.findViewById<ImageButton>(R.id.previousMonthButton)
+        previousMonthBtn.setOnClickListener{
+            dashboardViewModel.previousMonth()
+        }
+
+        val nextMonthBtn = root.findViewById<ImageButton>(R.id.nextMonthButton)
+        nextMonthBtn.setOnClickListener{
+            dashboardViewModel.nextMonth()
+        }
+
         return root
     }
+
+
+
 }
--- app/src/main/java/com/example/budget/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/budget/ui/dashboard/DashboardViewModel.kt
@@ -9,6 +9,12 @@ import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
 class DashboardViewModel : ViewModel() {
+    //TODO
+    @RequiresApi(Build.VERSION_CODES.O)
+    var actual = LocalDateTime.now()
+    //TODO
+    @RequiresApi(Build.VERSION_CODES.O)
+    var formatter = DateTimeFormatter.ofPattern("MMMM-yyyy")
 
     private val _text = MutableLiveData<String>().apply {
         value = "Gestion de budget"
@@ -18,13 +24,23 @@ class DashboardViewModel : ViewModel() {
     //TODO
     @RequiresApi(Build.VERSION_CODES.O)
     private val _currentMonth = MutableLiveData<String>().apply {
-        val current = LocalDateTime.now()
-        var formatter = DateTimeFormatter.ofPattern("MMMM-yyyy")
-        var formattedDate = current.format(formatter)
-
+        var formattedDate = actual.format(formatter)
         value = formattedDate.toString()
     }
     //TODO
     @RequiresApi(Build.VERSION_CODES.O)
     val currentMonth: LiveData<String> = _currentMonth
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun previousMonth(){
+        actual = actual.minusMonths(1);
+        _currentMonth.value = actual.format(formatter);
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun nextMonth(){
+        actual = actual.plusMonths(1);
+        _currentMonth.value = actual.format(formatter);
+    }
+
 }
