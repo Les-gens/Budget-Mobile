397d51cf4c971415f387f127b8c2d64c5934a4bd
Francois Lannoy <flannoy154@gmail.com>
2021-05-30 22:53:14 +0200

    :confetti_ball: OwO

--- app/src/main/java/com/example/budget/models/EntriesModel.kt
+++ app/src/main/java/com/example/budget/models/EntriesModel.kt
@@ -5,4 +5,8 @@ import java.io.Serializable
 
 class EntriesModel (var amount: Double, var title: String, Tag: String, var date: String): Serializable{
     constructor():this(0.0,"Poet","Poet", "18/11/1999")
+
+    override fun toString(): String {
+        return amount.toString() + " " + title.toString()
+    }
 }
