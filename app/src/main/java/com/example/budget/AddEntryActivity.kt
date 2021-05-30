5c12438399147677b6e2bb29d1e26fd10260e00b
Francois Lannoy <flannoy154@gmail.com>
2021-05-31 01:40:43 +0200

    :construction: working on add_entry

--- app/src/main/java/com/example/budget/AddEntryActivity.kt
+++ app/src/main/java/com/example/budget/AddEntryActivity.kt
@@ -2,16 +2,48 @@ package com.example.budget
 
 import android.content.Intent
 import android.os.Bundle
-import android.os.PersistableBundle
+import com.example.budget.MainActivity
+import android.text.Editable
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import com.example.budget.ui.auth.SignUpActivity
-import kotlinx.android.synthetic.main.activity_sign_in.*
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModelProvider
+import com.example.budget.models.EntriesModel
+import com.example.budget.repositories.EntriesRepository
+import com.example.budget.ui.dashboard.DashboardViewModel
+import kotlinx.android.synthetic.main.activity_add_entry.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 class AddEntryActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var actual = LocalDateTime.now()
+
+        var formatter = DateTimeFormatter.ofPattern("MM-yyyy")
+        var formattedDate = actual.format(formatter)
+
         setContentView(R.layout.activity_add_entry)
 
+        submit_entry.setOnClickListener {
+            var title = title_form.text.toString()
+            var amount: Double = amount_form.text.toString().toDouble()
+            var date: String = date_form.text.toString().format(formatter)
+            var dashboardViewModel =
+                ViewModelProvider(this).get(DashboardViewModel::class.java)
+            saveEntryToFireBase(EntriesModel(amount, title, "", ""), date)
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
+    }
+    var repository = EntriesRepository()
+
+    fun saveEntryToFireBase(model: EntriesModel, currentDate: String) {
+
+        repository.saveEntry(model, currentDate).addOnFailureListener {
+            Log.v("FAILED_ENTRY_SAVE","Failed to save Entry!")
+        }
 
     }
 }
