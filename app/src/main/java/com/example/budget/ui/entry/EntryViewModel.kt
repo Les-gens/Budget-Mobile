cd9fe5cb36d6fac8ac098ce7ca52048a98c9fb02
Francois Lannoy <flannoy154@gmail.com>
2021-05-30 15:52:10 +0200

    :construction: WIP firebase get entries

--- app/src/main/java/com/example/budget/ui/entry/EntryViewModel.kt
+++ app/src/main/java/com/example/budget/ui/entry/EntryViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.budget.ui.entry
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.budget.utils.FirebaseUtils.db
 
 class EntryViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
@@ -15,10 +17,21 @@ class EntryViewModel : ViewModel() {
         value = "15€"
     }
     private val _text4 = MutableLiveData<String>().apply {
-        value = "Super robot"
+        value = ""
     }
     val text1: LiveData<String> = _text
     val text2: LiveData<String> = _text2
     val text3: LiveData<String> = _text3
     val text4: LiveData<String> = _text4
+
+    val res = db.collection("entries")
+    .get()
+    .addOnSuccessListener { result ->
+        for (document in result) {
+            Log.d(TAG, "${document.id} => ${document.data}")
+        }
+    }
+    .addOnFailureListener { exception ->
+        Log.w(TAG, "Error getting documents.", exception)
+    }
 }
--- app/src/main/java/com/example/budget/utils/FirebaseUtils.kt
+++ app/src/main/java/com/example/budget/utils/FirebaseUtils.kt
@@ -2,9 +2,15 @@ package com.example.budget.utils
 
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.firestore.ktx.firestore
 
 
 object FirebaseUtils {
+    val db = Firebase.firestore
+
     val firebaseAuth: FirebaseAuth = FirebaseAuth.getInstance()
     val firebaseUser: FirebaseUser? = firebaseAuth.currentUser
+
+
 }
