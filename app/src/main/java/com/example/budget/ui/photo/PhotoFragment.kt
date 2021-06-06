9207a60f52b4f96fcb74ca4bbf3b975e46c8a96f
verzelea <arthurverzele@hotmail.com>
2021-06-06 20:07:23 +0200

    Error: add Photo

--- app/src/main/java/com/example/budget/ui/photo/PhotoFragment.kt
+++ app/src/main/java/com/example/budget/ui/photo/PhotoFragment.kt
@@ -1,14 +1,28 @@
 package com.example.budget.ui.photo
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import androidx.core.app.ActivityCompat
 import com.example.budget.R
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.util.*
+
 
 class PhotoFragment : Fragment() {
+    private lateinit var button:ImageButton
 
     companion object {
         fun newInstance() = PhotoFragment()
@@ -20,13 +34,67 @@ class PhotoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_photo, container, false)
+        var root = inflater.inflate(R.layout.fragment_photo, container, false)
+        button = root.findViewById<ImageButton>(R.id.btnPhoto)
+        button.isEnabled = false
+
+        if(ActivityCompat.checkSelfPermission(root.context, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(Activity(), arrayOf(android.Manifest.permission.CAMERA), 111)
+        }else{
+            button.isEnabled = true
+            button.setOnClickListener{
+                var i = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+                startActivityForResult(i, 101)
+            }
+        }
+        return root
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+            button.isEnabled = true
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==101){
+            var pic = data?.getParcelableExtra<Bitmap>("data")
+            var path = saveToInternalStorage(pic!!)
+        }
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProvider(this).get(PhotoViewModel::class.java)
         // TODO: Use the ViewModel
+
     }
 
+    private fun saveToInternalStorage(bitmapImage: Bitmap): String? {
+        //val cw = ContextWrapper()
+
+        val directory: File = context?.getDir("imageDir", Context.MODE_PRIVATE) ?: return null
+        var tmp = UUID.randomUUID().toString()
+        val mypath = File(directory, "profile_$tmp.jpg")
+        var fos: FileOutputStream? = null
+        try {
+            fos = FileOutputStream(mypath)
+            bitmapImage.compress(Bitmap.CompressFormat.PNG, 100, fos)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            try {
+                fos?.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return directory.getAbsolutePath()
+    }
 }
