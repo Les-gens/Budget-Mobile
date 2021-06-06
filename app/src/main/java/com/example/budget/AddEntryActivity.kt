4c5cedbf99f0e1270c25b3a539d9987f50004add
Francois Lannoy <flannoy154@gmail.com>
2021-06-06 16:40:27 +0200

    :poop:

--- app/src/main/java/com/example/budget/AddEntryActivity.kt
+++ app/src/main/java/com/example/budget/AddEntryActivity.kt
@@ -51,9 +51,9 @@ class AddEntryActivity : AppCompatActivity() {
 
         val vibrator = getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
         if (Build.VERSION.SDK_INT >= 26) {
-            vibrator.vibrate(VibrationEffect.createOneShot(200, VibrationEffect.DEFAULT_AMPLITUDE))
+            vibrator.vibrate(VibrationEffect.createOneShot(4000, VibrationEffect.DEFAULT_AMPLITUDE))
         } else {
-            vibrator.vibrate(200)
+            vibrator.vibrate(4000)
         }
     }
 
